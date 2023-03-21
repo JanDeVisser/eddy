d4c050cf63c7881c9cafed21652646b3bc427fd3
Jan de Visser <jan@de-visser.net>
2023-03-21 08:11:06 -0400

    Some more new tests for various language features

--- Test/ScribbleTest.cpp
+++ Test/ScribbleTest.cpp
@@ -16,6 +16,15 @@ TEST_F(ScribbleTest, Var)
     EXPECT_EQ(stmt.result, Value { 42 });
 }
 
+TEST_F(ScribbleTest, Assign)
+{
+    Scratch::ConsoleStatement stmt;
+    stmt.compile("var x = 42\nx = 12\nx\n");
+    EXPECT_NE(stmt.node, nullptr);
+    stmt.execute(m_ctx);
+    EXPECT_EQ(stmt.result, Value { 12 });
+}
+
 TEST_F(ScribbleTest, VarAdd)
 {
     Scratch::ConsoleStatement stmt;
@@ -32,6 +41,44 @@ TEST_F(ScribbleTest, VarMult)
     EXPECT_NE(stmt.node, nullptr);
     stmt.execute(m_ctx);
     EXPECT_EQ(stmt.result, Value { 108 });
+}
+
+TEST_F(ScribbleTest, FuncDef)
+{
+    Scratch::ConsoleStatement stmt;
+    stmt.compile("func foo(x) { return 2*x }");
+    EXPECT_NE(stmt.node, nullptr);
+    stmt.execute(m_ctx);
+    EXPECT_EQ(stmt.result.type(), ValueType::Null);
+}
+
+TEST_F(ScribbleTest, FuncCall)
+{
+    Scratch::ConsoleStatement stmt;
+    stmt.compile("func foo(x) { return 2*x }   foo(4)");
+    EXPECT_NE(stmt.node, nullptr);
+    stmt.execute(m_ctx);
+    EXPECT_EQ(stmt.result, Value { 8 });
+}
+
+TEST_F(ScribbleTest, If)
+{
+    Scratch::ConsoleStatement stmt;
+    stmt.compile("var x = 2 var y = 0 if x > y { y = 3 } else { y = 4 } y");
+    EXPECT_NE(stmt.node, nullptr);
+    stmt.execute(m_ctx);
+    EXPECT_EQ(stmt.result.type(), ValueType::Integer);
+    EXPECT_EQ(stmt.result.to_int<int>(), 3);
+}
+
+TEST_F(ScribbleTest, While)
+{
+    Scratch::ConsoleStatement stmt;
+    stmt.compile("var x = 2 var y = 0 while y < 10 { x = x + y y = y + 1 } x");
+    EXPECT_NE(stmt.node, nullptr);
+    stmt.execute(m_ctx);
+    EXPECT_EQ(stmt.result.type(), ValueType::Integer);
+    EXPECT_EQ(stmt.result.to_int<int>(), 47);
 }
 
 } // scratch::test
